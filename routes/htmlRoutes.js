59aa4debb9220d3761a1fab13e1fc240b6f9b421
Sally Alvarenga <sallyalvarenga35@gmail.com>
2021-09-14 08:41:12 -0500

    fixed end point and heroku link

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,13 @@ const htmlRoutes = require('express').Router();
 const path  = require('path');
 
 // GET Routes for HTML Notes & index page
-htmlRoutes.get('notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','notes.html'));
+htmlRoutes.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
     // catch all is star
 htmlRoutes.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
     );
 
 module.exports = htmlRoutes;
